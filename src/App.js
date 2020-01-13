b0edc557d71cd53a2553b699068bcd979dedf4e2
Alexandre Gerez <nextriesh@gmail.com>
2020-01-13 12:26:00 +0100

    navBar + responsive

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Pages
+import Home from "./pages/Home/Home.js"
+
+//Components
+import NavBar from "./components/NavBar/NavBar.js"
+
+//Logo
+import Logo from "./assets/kick.jpg"
+
+export default class App extends React.Component {
+
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+      selectedPage: null
+    };
+  }
+
+  render()
+  {
+    return (
+      <div className="appContainer">
+        <NavBar logo={Logo} />
+        {this.state.selectedPage ? this.state.selectedPage : <Home />}
+      
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import "./NavBar.css"
+
+//Images
+import Burger from "../../assets/menu.png"
+
+const navElements = {
+    links:
+    [
+        {
+            link:
+            {
+                title: "Home",
+                link: "#"
+            }
+        },
+
+        {
+            link:
+            {
+                title: "PlaceHolder",
+                link: "#"
+            }
+        },
+
+        {
+            link:
+            {
+                title: "PlaceHolder",
+                link: "#"
+            }
+        }
+    ]
+}
+
+export default class NavBar extends React.Component
+{
+
+    constructor(props)
+    {
+        super(props);
+
+        this.showResponsiveMenu = this.showResponsiveMenu.bind(this);
+    }
+
+    getAppLogo()
+    {
+        if (this.props.logo)
+            return this.props.logo;
+    }
+
+    showResponsiveMenu()
+    {
+        var menu = this.refs.navMenu;
+        var isMenuVisible = window.getComputedStyle(menu).right === "0px";
+        
+        if (!isMenuVisible)
+            menu.style.right = "0px";
+        else
+            menu.style.right = "-405px";
+    }
+
+    render()
+    {
+        return (
+            <>
+                <div className="navBarContainer">
+                    <a href="#">
+                        <img className="appLogo" src={this.getAppLogo()} alt="logo"/>
+                    </a>
+                    <div className="navBarElementsContainer">
+                        <div ref="navMenu" className="navElements">
+                            {
+                                navElements.links.map(element => 
+                                {
+                                    return (
+                                        <a key={element.link.title} href={element.link.link}>
+                                            {element.link.title}
+                                        </a>
+                                    );
+                                })
+                            }
+                            <a href="http://app.kickapp.fr">
+                                <div className="roundedGetStartedButton">
+                                    Get Started
+                                </div>
+                            </a>
+                        </div>
+                        <div className="responsiveNavElement">
+                            <a onClick={this.showResponsiveMenu}>
+                                <img src={Burger} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
+
